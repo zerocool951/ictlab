3bfc80c40cc02ac6434173b3799d257b6cc77089
Hosman <hosman@soft-pay.com>
2015-06-09 15:03:20 +0200

    VpnService & MainActivity
    VpnService with basic vpn service connection, mainactivity with onclick
    of button to VPN.

---
+++ AndroidClient/src/ictlab/MainActivity.java
@@ -0,0 +1,41 @@
+package vpnservice.com.example.hk.traxvpn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.net.VpnService;
+
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    //Button vpn_start_button = (Button) findViewById(R.id.vpn_action_button);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.vpn_action_button).setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+        Intent intent = VpnService.prepare(getApplicationContext());
+
+        if(intent != null){
+            startActivityForResult(intent, 0);
+        }else{
+            onActivityResult(0, RESULT_OK, null);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if(resultCode == RESULT_OK){
+            Intent intent = new Intent(this, TraxVpnService.class);
+            startService(intent);
+        }
+    }
+}
---
+++ AndroidClient/src/ictlab/TraxVpnService.java
@@ -0,0 +1,115 @@
+package vpnservice.com.example.hk.traxvpn;
+
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+
+/**
+ * Created by HK on 9-6-2015.
+ */
+public class TraxVpnService extends VpnService{
+    private String serverAddress = "185.77.129.237";
+    private int serverPort = 3306;
+    private String serverDns = "8.8.8.8";
+    private String serverRoute = "0.0.0.0";
+
+    private Thread mThread;
+    private ParcelFileDescriptor mInterface;
+    Builder builder = new Builder();
+
+    public int runVpn(Intent intent, int flags, int beginId) throws Exception{
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    InetSocketAddress server = new InetSocketAddress(serverAddress, serverPort);
+                    //Configuring the builder interface
+                   mInterface = builder.setSession("TraxVpn")
+                           .addAddress("192.168.0.1", 45)
+                           .addDnsServer(serverDns)
+                           .addRoute(serverRoute, 0).establish();
+                    //packets queued in input stream
+                    FileInputStream in = new FileInputStream(mInterface.getFileDescriptor());
+                    FileOutputStream out = new FileOutputStream(mInterface.getFileDescriptor());
+
+                    DatagramChannel tunnel = DatagramChannel.open();
+                    //connect to server
+                    tunnel.connect(server);
+                    protect(tunnel.socket());
+
+                    ByteBuffer packet = ByteBuffer.allocate(65535);
+                    int timer = 0;
+
+                    //pass the packets
+                    while(true){
+                        boolean idle = true;
+
+                        int length = in.read(packet.array());
+                        if(length > 0){
+                            if(packet.get(0) != 0){
+                                out.write(packet.array(), 0, length);
+                            }
+                            packet.clear();
+
+                            idle = false;
+
+                            if(timer > 0){
+                                timer = 0;
+                            }
+                        }
+
+                        if(idle){
+                            Thread.sleep(100);
+
+                            timer += (timer > 0) ? 100 : -100;
+
+                            if(timer < -15000){
+                                packet.put((byte) 0).limit(1);
+                                for(int i = 0; i < 3; ++i){
+                                    packet.position(0);
+                                    tunnel.write(packet);
+                                }
+                                packet.clear();
+                                timer = 1;
+                            }
+
+                            if(timer > 20000){
+                                throw new IllegalArgumentException("Timed out");
+                            }
+                        }
+                    }
+
+
+                } catch (Exception e){
+                    Log.i ("error: ", e.toString());
+                } finally{
+                    try{
+                        if(mInterface != null){
+                            mInterface.close();
+                            mInterface = null;
+                        }
+                    } catch(Exception e){
+                        Log.i ("error: ", e.toString());
+                    }
+                }
+            }
+        }, "TraxVpn");
+        mThread.start();
+        return START_STICKY;
+    }
+
+    public void onDestroy(){
+        if(mThread != null){
+            mThread.interrupt();
+        }
+
+        super.onDestroy();
+    }
+}
